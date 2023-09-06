54b10a88b45fcf06eb19f84ceb5b01b637592208
Larissa Sevilha <sevilha.larissa10@gmail.com>
2023-09-06 14:49:38 -0400

    native browser features

---
+++ aula10/js/index.js
@@ -0,0 +1,14 @@
+let nome = 'Larissa'
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+
+alert(nome)
+nome = window.prompt("Insira o seu nome")
+
+let confirm = window.confirm("Confirma essa ação? ")
+console.log(confirm) 
+
+alert("Olá")
